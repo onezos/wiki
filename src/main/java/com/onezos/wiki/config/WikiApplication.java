def3890d914ad0f02511e54efbb3f2b846fea107
onezos <gdd9399@gmail.com>
2021-03-21 20:34:11 +0800

    2-6 开发Hello World接口 访问地址：http://127.0.0.1:8880/hello

--- src/main/java/com/onezos/wiki/config/WikiApplication.java
+++ src/main/java/com/onezos/wiki/config/WikiApplication.java
@@ -1,12 +1,13 @@
-package com.onezos.wiki;
+package com.onezos.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
-
+@ComponentScan("com.onezos")
 @SpringBootApplication
 public class WikiApplication {
 
