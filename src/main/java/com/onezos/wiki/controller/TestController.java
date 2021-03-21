6f2ab92ab88b54af1087f7a4f10bb608bd00b2df
onezos <gdd9399@gmail.com>
2021-03-21 21:00:58 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/onezos/wiki/controller/TestController.java
+++ src/main/java/com/onezos/wiki/controller/TestController.java
@@ -1,8 +1,11 @@
 package com.onezos.wiki.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class TestController {
@@ -10,5 +13,10 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post. " + name;
     }
 }
