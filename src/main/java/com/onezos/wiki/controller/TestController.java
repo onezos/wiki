f9091b724dba4d91eb82dbcdd37d49f1425be1b0
onezos <gdd9399@gmail.com>
2021-03-23 11:13:51 +0800

    3-4 mybatis使用示例：开发/test/list接口，查询test表所有数据

--- src/main/java/com/onezos/wiki/controller/TestController.java
+++ src/main/java/com/onezos/wiki/controller/TestController.java
@@ -1,17 +1,23 @@
 package com.onezos.wiki.controller;
 
+import com.onezos.wiki.domain.Test;
+import com.onezos.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class TestController {
     @Value("${test.hello:TEST}")
     private String testHello;
+
+    @Resource
+    private TestService testService;
 
     @RequestMapping("/hello")
     public String hello() {
@@ -21,5 +27,10 @@ public class TestController {
     @PostMapping("/hello/post")
     public String helloPost(String name) {
         return "Hello World! Post. " + name;
+    }
+
+    @GetMapping("/test/list")
+    public List<Test> list() {
+        return testService.list();
     }
 }
---
+++ src/main/java/com/onezos/wiki/mapper/TestMapper.java
@@ -0,0 +1,10 @@
+package com.onezos.wiki.mapper;
+
+import com.onezos.wiki.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+
+    public List<Test> list();
+}
