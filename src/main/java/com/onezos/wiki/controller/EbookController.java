62cd8cc1a8b458a627fd81c3a5b850ac6eeea9cf
onezos <gdd9399@gmail.com>
2021-03-23 16:25:58 +0800

    3-6 增加通用的返回类CommonResp，以后所有后端接口的返回值全部都是CommonResp类型

--- src/main/java/com/onezos/wiki/controller/EbookController.java
+++ src/main/java/com/onezos/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.onezos.wiki.controller;
 
 import com.onezos.wiki.domain.Ebook;
+import com.onezos.wiki.resp.CommonResp;
 import com.onezos.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,11 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
+
     }
 }
