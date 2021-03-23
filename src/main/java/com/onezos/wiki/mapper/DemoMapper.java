2e528abaf5b5ff73f0c6bea556dd06211ef2b560
onezos <gdd9399@gmail.com>
2021-03-23 13:47:12 +0800

    3-5 增加demo表列表查询

--- src/main/java/com/onezos/wiki/mapper/DemoMapper.java
+++ src/main/java/com/onezos/wiki/mapper/DemoMapper.java
@@ -2,9 +2,8 @@ package com.onezos.wiki.mapper;
 
 import com.onezos.wiki.domain.Demo;
 import com.onezos.wiki.domain.DemoExample;
-import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface DemoMapper {
     long countByExample(DemoExample example);
---
+++ src/main/java/com/onezos/wiki/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.onezos.wiki.service;
+
+import com.onezos.wiki.domain.Demo;
+import com.onezos.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list() {
+        return demoMapper.selectByExample(null);
+    }
+}
