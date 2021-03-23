a0cd08be370364db13f43404ee403687472f2abd
onezos <gdd9399@gmail.com>
2021-03-23 22:16:36 +0800

    3-7 封装请求参数和返回参数

--- src/main/java/com/onezos/wiki/controller/EbookController.java
+++ src/main/java/com/onezos/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.onezos.wiki.controller;
 
-import com.onezos.wiki.domain.Ebook;
+import com.onezos.wiki.req.EbookReq;
 import com.onezos.wiki.resp.CommonResp;
+import com.onezos.wiki.resp.EbookResp;
 import com.onezos.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +19,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list() {
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req) {
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
 
