f79f9bce331f0ee4d45268e1526aa05b0c9a7ff4
onezos <gdd9399@gmail.com>
2021-03-23 11:14:27 +0800

    3-4 mybatis使用示例：开发/test/list接口，查询test表所有数据 新添加3个acs文件

---
+++ src/main/java/generate/Test.java
@@ -0,0 +1,26 @@
+package generate;
+
+import java.io.Serializable;
+
+/**
+ * test
+ * @author 
+ */
+public class Test implements Serializable {
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/generate/TestDao.java
@@ -0,0 +1,17 @@
+package generate;
+
+import generate.Test;
+
+public interface TestDao {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Test record);
+
+    int insertSelective(Test record);
+
+    Test selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Test record);
+
+    int updateByPrimaryKey(Test record);
+}
