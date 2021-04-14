ba2b71f68e7cbb9f07e3f593710a107e163b6f90
onezos <gdd9399@gmail.com>
2021-04-14 08:12:50 +0800

    6-5 封装分页请求参数PageReq和返回参数PageResp

--- src/main/java/com/onezos/wiki/controller/EbookController.java
+++ src/main/java/com/onezos/wiki/controller/EbookController.java
@@ -3,13 +3,13 @@ package com.onezos.wiki.controller;
 import com.onezos.wiki.req.EbookReq;
 import com.onezos.wiki.resp.CommonResp;
 import com.onezos.wiki.resp.EbookResp;
+import com.onezos.wiki.resp.PageResp;
 import com.onezos.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/ebook")
@@ -20,8 +20,8 @@ public class EbookController {
 
     @GetMapping("/list")
     public CommonResp list(EbookReq req) {
-        CommonResp<List<EbookResp>> resp = new CommonResp<>();
-        List<EbookResp> list = ebookService.list(req);
+        CommonResp<PageResp<EbookResp>> resp = new CommonResp<>();
+        PageResp<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
 
